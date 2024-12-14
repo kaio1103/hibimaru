515ef7867edd50fff5f910d59bd8b1630b752170
kaio1103 <orikikai@gmail.com>
2024-12-14 12:19:05 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hibimaru
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/kaio1103/hibimaru)
---
+++ src/config/images.ts
@@ -0,0 +1,12 @@
+export const imageConfig = {
+  background: {
+    publicId: 'background2',
+  },
+  character: {
+    talking: 'character/hwifbcqncg6wx8hkgd0k',
+    listening: 'character/lj8jttlkk0xpkvq8b8sl',
+  },
+  consent: {
+    character: 'character/l0dxbsxos3ex6',
+  },
+} as const;
